5ed682499dfe796b0bfc1afbc1f2a3c6050b91eb
vanessa082 <wahvanessa22@gmail.com>
2024-11-03 00:46:46 +0100

    finished the footer section

--- web/src/core/components/home-page/footer.tsx
+++ web/src/core/components/home-page/footer.tsx
@@ -3,26 +3,24 @@ import { Textlogo } from "../atoms";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-between gap-8 pt-4 ">
-      <div className="flex flex-col md:flex-row justify-between items-center">
-        <Textlogo includeLogo className="hidden sm:flex text-2xl"
-          
-          sxImg="size-[60px] sm:size-[50px]" />
-        <nav className="hidden md:flex justify-between gap-12 ">
+    <footer className="flex flex-col justify-between items-center gap-8 mt-8 ">
+      <div className="w-full flex flex-col md:flex-row justify-between items-center gap-4 md:gap-12">
+        <Textlogo className=" text-2xl" sxImg="size-[60px] sm:size-[50px]" />
+        <nav className="flex justify-between gap-12 ">
           <a href="">Demo</a>
           <a href="">About</a>
           <a href="">Contact</a>
         </nav>
-        <div className="flex justify-between items-center text-app-blue-500 gap-8">
+        <div className="flex justify-between items-center text-app-blue-500 gap-4">
           <X />
           <Facebook />
           <Instagram />
           <Github />
         </div>
       </div>
-      <div className="flex flex-col md:flex-row justify-between  text-app-text-dark-200">
-        <span>&copy;Copyright all right reserved</span>
-        <div className="flex justify-between items-center ">
+      <div className="w-full flex flex-col md:flex-row justify-between items-center pt-4 border-t border-solid  gap-2 text-app-text-dark-200">
+        <span className="">&copy;Copyright all right reserved</span>
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4">
           <h4>Privacy Policy</h4>
           <h4>Terms and Condition</h4>
         </div>
--- web/src/core/components/home-page/hero-section.tsx
+++ web/src/core/components/home-page/hero-section.tsx
@@ -7,48 +7,45 @@ import { ArrowRight, Star } from "lucide-react";
 export default function Herosection() {
   return (
     <section className="w-full flex flex-col-reverse md:flex-row justify-between items-center gap-4">
-      <div className="w-full flex-1 flex flex-col gap-6 ">
-        <div className="w-full font-bold text-5xl lg:text-6xl xl:text-7xl break-all">
-          <div>
-            <span className="flex gap-2 whitespace-nowrap">
-              Stay <Textlogo />,
-            </span>
-            Be Involved
-          </div>
+      <div className="w-full flex-1 flex flex-col items-center justify-center md:items-start gap-6 text-center md:text-start">
+        <div className="w-fit font-bold text-5xl lg:text-6xl xl:text-7xl break-all">
+          <span className="flex gap-2 whitespace-nowrap">
+            Stay <Textlogo />,
+          </span>
+          Be Involved
         </div>
         <p className="max-w-[400px]">
-          &quote;Discover your community. Share your voice. Never miss a moment.&quote; With InTouch, you re always part of the conversation
+          <q>Discover your community. Share your voice. Never miss a moment.</q> With InTouch, you re always part of the conversation
         </p>
 
         <Button className="w-fit flex">
-          <Link href="/feed" className="w-fit flex items-center gap-8">
+          <Link href="/feed" className="w-fit flex items-center gap-4">
             Be InTouch Now <ArrowRight />
           </Link>
         </Button>
 
-        <div className="flex justify-between items-center  w-full md:max-w-[35vw] ">
-          <Image src="./home/images.svg" alt="images" width={70} height={100}/>
-          <div className="flex flex-col">
-              <span className="font-bold">2,291</span>
+        <div className="hidden md:flex flex-row justify-between items-center gap-4 w-fit md:max-w-[35vw]">
+          <div className="w-fit flex items-center justify-center gap-4">
+            <Image src="./home/avatars.svg" alt="images" width={70} height={100} />
+            <div className="flex flex-col">
+              <span className="font-bold">000,2</span>
               <span className="text-app-dark-200">Happy user</span>
+            </div>
           </div>
-          <Image
-          src="/home/line.png"
-          alt="line"
-          height={4}
-          width={4}
-        />
+
+          <hr className="w-[2px] h-[60px] bg-app-dark-300 border-none hidden md:flex" />
+
           <div>
-              <span className="font-bold">4.8/5</span>
-              <div className="flex">
-                <Star color="yellow-400" className="fill-yellow-400"/>
-                <Star color="yellow-400" className="fill-yellow-400"/>
-                <Star color="yellow-400" className="fill-yellow-400"/>
-                <Star color="yellow-400" className="fill-yellow-400"/>
-                <Star color="yellow-400" className="fill-yellow-400"/>
-                <Star />
-              </div>
-              <span  className="text-app-dark-200">Rating</span>
+            <span className="font-bold">4.8/5</span>
+            <div className="flex">
+              {
+                Array.from({ length: 4 }, (_, i) => i).map((v) => (
+                  <Star key={v} color="yellow-400" className="fill-yellow-400" />
+                ))
+              }
+              <Star />
+            </div>
+            <span className="text-app-dark-200">Rating</span>
           </div>
         </div>
       </div>
