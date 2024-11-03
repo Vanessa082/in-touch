ffcacf2955382e8be448093129d31ee40ceacada
Rash <orashusedmund@gmail.com>
2024-11-03 01:03:07 +0100

    did the clerk intergration

--- web/README.md
+++ web/README.md
@@ -1,1 +1,5 @@
 # In Touch Web
+
+### Dev setup
+
+- Create `.env.local` file in root of `web` folder and add the same variables in the `.env.example` file
--- web/src/app/feed/page.tsx
+++ web/src/app/feed/page.tsx
@@ -1,3 +1,24 @@
-export default function FeedPage() {
-  return <div>FeedPage</div>;
-}
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export default async function FeedPage() {
+  const _ = await auth();
+
+  let user = null;
+
+  if (_?.userId) {
+    user = await currentUser();
+  }
+  
+  return (
+    <div className="">
+      Landing Page
+
+      <pre>
+        {JSON.stringify({
+          _,
+          user,
+        }, null, 2)}
+      </pre>
+    </div>
+  );
+};
