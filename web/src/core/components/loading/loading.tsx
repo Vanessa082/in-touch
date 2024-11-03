eecaaf7959ab221afe2e0d0969b5ac78cfdf5157
vanessa082 <wahvanessa22@gmail.com>
2024-11-03 20:51:28 +0100

    added the theme

--- web/src/core/components/loading/loading.tsx
+++ web/src/core/components/loading/loading.tsx
@@ -1,14 +1,16 @@
+"use client"
+
 import Image from "next/image";
 import { TextLogo } from "../atoms";
 import { useTheme } from "next-themes"
 
 export default function Loading() {
-  const { theme } = useTheme()
+  const { theme } = useTheme();
   return (
-    <div>
-      <Image src={theme === "light" ? "/logo-light.svg" : "/logo-dark.svg"} alt="" width={100} height={100} />
-      <div>
-        <span>Stay</span>
+    <div className="flex flex-col justify-center items-center gap-4 h-screen w-app-w">
+      <Image src={theme === "dark" ? "/logo-dark.svg" : "/logo-light.svg"} alt="" width={100} height={100} />
+      <div className="flex justify-between items-center gap-8 text-4xl ">
+        <span className="font-bold">Stay</span>
         <TextLogo />
       </div>
     </div>
