bd783007667681c1bbf46e51ebdd373614947d6d
vanessa082 <wahvanessa22@gmail.com>
2024-11-03 20:55:03 +0100

    added the theme

--- web/src/core/components/loading/loading.tsx
+++ web/src/core/components/loading/loading.tsx
@@ -7,7 +7,7 @@ import { useTheme } from "next-themes"
 export default function Loading() {
   const { theme } = useTheme();
   return (
-    <div className="flex flex-col justify-center items-center gap-4 h-screen w-app-w">
+    <div className="flex flex-col justify-center items-center gap-4 h-screen w-screen">
       <Image src={theme === "dark" ? "/logo-dark.svg" : "/logo-light.svg"} alt="" width={100} height={100} />
       <div className="flex justify-between items-center gap-8 text-4xl ">
         <span className="font-bold">Stay</span>
