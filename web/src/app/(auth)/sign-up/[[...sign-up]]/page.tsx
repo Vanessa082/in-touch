7512b76ceb49f68ee5debf168945d28b1e511560
vanessa082 <wahvanessa22@gmail.com>
2024-11-03 14:23:40 +0100

    created a togglomode ui and used the dropdown menu from shatcn to buld it, also, I went ahead to install next themes creating a theme provider withing a created theme folder in the core folder, added the theme provider as  a wrapper to the entire app by pasing the children prop usring the spread prop I added the attribute.

--- web/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ web/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SIgnUpPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center py-4">
+    <div className="flex min-h-screen items-center justify-center py-4">
       <SignUp />
     </div>
   );
